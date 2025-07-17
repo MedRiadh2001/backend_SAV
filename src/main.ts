b36714c0418ebb9ed0b4f834ced86827136f54b7
MedRiadh2001 <mohamedriadh.essridi@polytechnicien.tn>
2025-07-17 10:34:36 +0100

    Fix

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors()
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
